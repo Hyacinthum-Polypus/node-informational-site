67217dd96b453758c1256738bb5525c12610841f
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2025-02-06 14:10:01 +1100

    Add content and logic

---
+++ index.js
@@ -0,0 +1,37 @@
+import http from 'node:http'
+import fs from 'fs/promises'
+
+const server = http.createServer((req, res) => {
+    switch(req.path) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.readFile('./index.html', 'utf-8')
+            .then(data => {
+                res.end(data);
+            })
+        break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.readFile('./about.html', 'utf-8')
+            .then(data => {
+                res.end(data);
+            })
+        break;
+        case 'contact-me':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.readFile('./contact-me.html', 'utf-8')
+            .then(data => {
+                res.end(data);
+            })
+        break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            fs.readFile('./404.html', 'utf-8')
+            .then(data => {
+                res.end(data);
+            })
+        break;
+    }
+})
+
+server.listen(8080);
